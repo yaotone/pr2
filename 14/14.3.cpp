1ccc57c4c9c0f5c712bb6aa89d733f63cf159eec
yaotone <125026669+yaotone@users.noreply.github.com>
2023-05-27 14:45:19 +0300

    Add files via upload

---
+++ 14/14.3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include<vector>
+#include <stdexcept>
+using namespace std;
+int main() {
+    vector<int> v{14,543,758,234,56};
+    int val;
+    try {
+        cout << "vvedite znachenie kotoroe nado naity: ";
+        cin >> val;
+        if (find(v.begin(),v.end(), val)==v.end()) {
+            throw runtime_error("znacheniya net");
+        }
+        cout << "znachenie " << val << " naideno" << "\n";
+    }
+    catch (runtime_error& e) {
+        cout << "exception accured\n" << e.what();
+    }
+    return 0;
+}
