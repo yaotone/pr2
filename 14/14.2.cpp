ad95587af325a0aac515f9dcab1a5b34d0d12bb5
yaotone <125026669+yaotone@users.noreply.github.com>
2023-05-27 14:32:40 +0300

    Add files via upload

---
+++ 14/14.2.cpp
@@ -0,0 +1,19 @@
+﻿using namespace std;
+#include <iostream>
+#include <stdexcept>
+int main() 
+{
+    int i;
+    int arr[5] = {9,534,123,64,67 };
+    try {
+        cout << "vvedite index: ";
+        cin >> i;
+        if (i < 0 || i >= size(arr)) {
+            throw runtime_error("necorrectnii index\n");
+        }
+    }
+    catch (runtime_error& e) {
+        cout <<"exception occured\n" << e.what() << "\n";
+    }
+    return 0;
+}
